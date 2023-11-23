b5a233d52e2ca14a8b0878134b5b72cb090c26f8
Anton Laidmets <antonlaidmets@gmail.com>
2023-11-23 20:07:48 +0200

    Enam ei pea Spotifys laul käima, et mäng töötaks

--- main_wSpotify(vol2).py
+++ main_wSpotify(vol2).py
@@ -3,12 +3,16 @@ from pygame import mixer
 import math
 import random
 import sys
+import time
+import random
 
 #spotify import
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-#import os
 
+import os
+#vahetab working directory faili asukoha directoriks
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 """
 #id-d environment id-d (seda pole tegelt vaja aga mingi tutorial soovitas)
 os.environ['CLIENT_ID'] = '93634ca4f50c4971827d1bd3cb13ce5d'
@@ -36,8 +40,22 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id = client_id,
 )
 #preagu mängiva laulu andmed
 def get_current_track():
+    ooteaeg = 1#sekundites
+    result = None
     result = sp.current_user_playing_track()
-    return result
+    if result != None:
+        return result
+    else:
+        print('Pane Spotifys laul käima')
+        start_time = time.monotonic()
+        while result == None and (start_time + ooteaeg > time.monotonic()):
+            result = None
+            result = sp.current_user_playing_track()
+        if result == None:
+            print('Sa ei pannud Spotifys laulu käima')
+        return result
+        
+    
 #saab laulu andmed nagu tempo, liveliness, energy jne
 def get_audio_features(song_id):
    result = sp.audio_features(song_id)
@@ -50,7 +68,7 @@ FPS = 60
 
 #mängu ekraan
 laius = 747
-pikkus = 980
+pikkus = 500
 ekraan = pygame.display.set_mode((laius, pikkus))
 pygame.display.set_caption("Proge mäng")
 
@@ -60,11 +78,10 @@ taustapilt_rect = taustapilt.get_rect()
 
 taustapilt_y = 0
 
-
 #auto, mängija
 auto = pygame.image.load("auto.png")
 mängija_asukoht1 = 320
-mängija_asukoht2 = 800
+mängija_asukoht2 = 400
 #auto seisab alguses
 mängija_asukoha_vahetus1 = 0
 mängija_asukoha_vahetus2 = 0
@@ -83,7 +100,6 @@ def takistus():
     ekraan.blit(heinapall,(heinapalli_asukoht1, heinapalli_asukoht2))
 
 #kui auto puutub heinakuhjaga kokku
-
 def põrge(mängija_asukoht1, mängija_asukoht2, heinapalli_asukoht1, heinapalli_asukoht2):
     if abs(mängija_asukoht1 - heinapalli_asukoht1) < 64 and abs(mängija_asukoht2 - heinapalli_asukoht2) < 64:
         global mängija_asukoha_vahetus1
@@ -97,15 +113,14 @@ def põrge(mängija_asukoht1, mängija_asukoht2, heinapalli_asukoht1, heinapalli
         mängu_lõpp()
 
 
-    
 #praeguse laulu saamine
 current_track = get_current_track()
-current_track_features = get_audio_features(current_track['item']['id'])
-print(current_track_features['tempo'])
 if current_track == None:
-    print('Pane Spotifys laul käima')
-    while current_track == None:
-        current_track = get_current_track()
+    current_track = {'timestamp': 1700760967441, 'context': {'external_urls': {'spotify': 'https://open.spotify.com/artist/0du5cEVh5yTK9QJze8zA0C'}, 'href': 'https://api.spotify.com/v1/artists/0du5cEVh5yTK9QJze8zA0C', 'type': 'artist', 'uri': 'spotify:artist:0du5cEVh5yTK9QJze8zA0C'}, 'progress_ms': 4680, 'item': {'album': {'album_type': 'album', 'artists': [{'external_urls': {'spotify': 'https://open.spotify.com/artist/0du5cEVh5yTK9QJze8zA0C'}, 'href': 'https://api.spotify.com/v1/artists/0du5cEVh5yTK9QJze8zA0C', 'id': '0du5cEVh5yTK9QJze8zA0C', 'name': 'Bruno Mars', 'type': 'artist', 'uri': 'spotify:artist:0du5cEVh5yTK9QJze8zA0C'}], 'available_markets': ['AE', 'AG', 'AL', 'AR', 'AT', 'BA', 'BB', 'BD', 'BE', 'BG', 'BH', 'BO', 'BR', 'BS', 'BZ', 'CH', 'CL', 'CO', 'CR', 'CW', 'CY', 'CZ', 'DE', 'DK', 'DM', 'DO', 'DZ', 'EC', 'EE', 'EG', 'ES', 'FI', 'FR', 'GB', 'GD', 'GR', 'GT', 'GY', 'HN', 'HR', 'HT', 'HU', 'ID', 'IE', 'IL', 'IN', 'IQ', 'IS', 'IT', 'JM', 'JO', 'JP', 'KN', 'KR', 'KW', 'LB', 'LC', 'LI', 'LK', 'LT', 'LU', 'LV', 'LY', 'MA', 'ME', 'MK', 'MT', 'MX', 'NI', 'NL', 'NO', 'OM', 'PA', 'PE', 'PK', 'PL', 'PS', 'PT', 'PY', 'QA', 'RO', 'RS', 'SA', 'SE', 'SG', 'SI', 'SK', 'SR', 'SV', 'TN', 'TR', 'TT', 'UA', 'UY', 'VC', 'VE', 'XK', 'ZA'], 'external_urls': {'spotify': 'https://open.spotify.com/album/6J84szYCnMfzEcvIcfWMFL'}, 'href': 'https://api.spotify.com/v1/albums/6J84szYCnMfzEcvIcfWMFL', 'id': '6J84szYCnMfzEcvIcfWMFL', 'images': [{'height': 640, 'url': 'https://i.scdn.co/image/ab67616d0000b273f60070dce96a2c1b70cf6ff0', 'width': 640}, {'height': 300, 'url': 'https://i.scdn.co/image/ab67616d00001e02f60070dce96a2c1b70cf6ff0', 'width': 300}, {'height': 64, 'url': 'https://i.scdn.co/image/ab67616d00004851f60070dce96a2c1b70cf6ff0', 'width': 64}], 'name': 'Doo-Wops & Hooligans', 'release_date': '2010-10-05', 'release_date_precision': 'day', 'total_tracks': 12, 'type': 'album', 'uri': 'spotify:album:6J84szYCnMfzEcvIcfWMFL'}, 'artists': [{'external_urls': {'spotify': 'https://open.spotify.com/artist/0du5cEVh5yTK9QJze8zA0C'}, 'href': 'https://api.spotify.com/v1/artists/0du5cEVh5yTK9QJze8zA0C', 'id': '0du5cEVh5yTK9QJze8zA0C', 'name': 'Bruno Mars', 'type': 'artist', 'uri': 'spotify:artist:0du5cEVh5yTK9QJze8zA0C'}], 'available_markets': ['AE', 'AG', 'AL', 'AR', 'AT', 'BA', 'BB', 'BD', 'BE', 'BG', 'BH', 'BO', 'BR', 'BS', 'BZ', 'CH', 'CL', 'CO', 'CR', 'CW', 'CY', 'CZ', 'DE', 'DK', 'DM', 'DO', 'DZ', 'EC', 'EE', 'EG', 'ES', 'FI', 'FR', 'GB', 'GD', 'GR', 'GT', 'GY', 'HN', 'HR', 'HT', 'HU', 'ID', 'IE', 'IL', 'IN', 'IQ', 'IS', 'IT', 'JM', 'JO', 'JP', 'KN', 'KR', 'KW', 'LB', 'LC', 'LI', 'LK', 'LT', 'LU', 'LV', 'LY', 'MA', 'ME', 'MK', 'MT', 'MX', 'NI', 'NL', 'NO', 'OM', 'PA', 'PE', 'PK', 'PL', 'PS', 'PT', 'PY', 'QA', 'RO', 'RS', 'SA', 'SE', 'SG', 'SI', 'SK', 'SR', 'SV', 'TN', 'TR', 'TT', 'UA', 'UY', 'VC', 'VE', 'XK', 'ZA'], 'disc_number': 1, 'duration_ms': 189109, 'explicit': False, 'external_ids': {'isrc': 'USAT21001886'}, 'external_urls': {'spotify': 'https://open.spotify.com/track/386RUes7n1uM1yfzgeUuwp'}, 'href': 'https://api.spotify.com/v1/tracks/386RUes7n1uM1yfzgeUuwp', 'id': '386RUes7n1uM1yfzgeUuwp', 'is_local': False, 'name': 'The Lazy Song', 'popularity': 81, 'preview_url': 'https://p.scdn.co/mp3-preview/8a6cd6679fc2a388b989a09b571194723d45cb71?cid=93634ca4f50c4971827d1bd3cb13ce5d', 'track_number': 5, 'type': 'track', 'uri': 'spotify:track:386RUes7n1uM1yfzgeUuwp'}, 'currently_playing_type': 'track', 'actions': {'disallows': {'resuming': True}}, 'is_playing': True}
+
+print(current_track)
+current_track_features = get_audio_features(current_track['item']['id'])
+#print(current_track_features['tempo'])
 print(f'Praegu mängib: {current_track['item']['name']}')
 
 #tekst ekraanile(praegu lihtsalt, et testida kas saab laulu)
@@ -119,8 +134,6 @@ def mängu_lõpp():
     väljund = mängu_lõpp_font.render("MÄNGU LÕPP", True, (255, 0, 0))
     ekraan.blit(väljund, (240, 400))
 
-
-        
 #tausta liikumiskiirus
 #paramaatrid
 tempoMin = 50
@@ -132,7 +145,6 @@ taustakiirusMax = 10
 taustakiirusRange = (taustakiirusMax - taustakiirusMin)
 #taustakiiruse arvutamine
 taustakiirus = (int(((float(current_track_features['tempo']) - tempoMin) * taustakiirusRange) / tempoRange) + taustakiirusMin)
-
 
 #mängu tsükkel
 running = True
